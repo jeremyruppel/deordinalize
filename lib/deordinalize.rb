0a18c1f5df280a4ae35a133fc8e30c67580b0c64
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2011-06-22 20:32:43 -0700

    think about how to set up these specs

--- lib/deordinalize.rb
+++ lib/deordinalize.rb
@@ -2,6 +2,20 @@ module Deordinalize
   # Your code goes here...
   
   def deordinalize
+    
+    case self.downcase
+    when "first"
+      0
+    when "second"
+      1
+    when "third"
+      2
+    when "fourth"
+      3
+    when "fifth"
+      4
+    end
+    
   end
   
 end
--- spec/deordinalize/deordinalize_spec.rb
+++ spec/deordinalize/deordinalize_spec.rb
@@ -1,8 +1,29 @@
 require 'spec_helper'
 
-describe "deordinalizing a string" do
-  
-  it "should define deordinalize" do
+describe "a string" do
+  it "should respond to deordinalize" do
     "hello".should respond_to(:deordinalize)
+  end
+  
+  context "when not an ordinal" do
+    it "should return nil" do
+      "hello".deordinalize.should be_nil
+    end
+  end
+  
+  %w|
+  
+  first
+  second
+  third
+  fourth
+  fifth
+  
+  |.each_with_index do |ordinal, index|
+    
+    it "should deordinalize '#{ordinal}' to #{index}" do
+      ordinal.deordinalize.should == index
+    end
+    
   end
 end
